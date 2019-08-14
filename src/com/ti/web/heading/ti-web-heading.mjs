265880c65293661303e74844a85dec3ff7b45301
Peter Zhang <zozohtnt@gmail.com>
2019-08-15 00:33:59 +0800

    ...

---
+++ src/com/ti/web/heading/ti-web-heading.mjs
@@ -0,0 +1,18 @@
+export default {
+  /////////////////////////////////////////
+  props : {
+    "title" : {
+      type : String,
+      default : null
+    },
+    "comment" : {
+      type : String,
+      default : null
+    }
+  },
+  //////////////////////////////////////////
+  computed : {
+    
+  }
+  //////////////////////////////////////////
+}
