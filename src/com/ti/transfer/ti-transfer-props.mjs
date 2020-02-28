94d17a9fca6f99a0a991fa03f74b799de253232a
Peter Zhang <zozohtnt@gmail.com>
2020-02-29 04:41:32 +0800

    ...

---
+++ src/com/ti/transfer/ti-transfer-props.mjs
@@ -0,0 +1,49 @@
+export default {
+  //-----------------------------------
+  // Data
+  //-----------------------------------
+  // +1 from the begin
+  // -1 from the last
+  "maxValueLen" : {
+    type : Number,
+    default : 0
+  },
+  "valueUnique" : {
+    type : Boolean,
+    default : true
+  },
+  "tagOptions" : {
+    type : [Array, Function],
+    default : ()=>[]
+  },
+  "tagMapping" : {
+    type : Object,
+    default : undefined
+  },
+  //-----------------------------------
+  // Behavior
+  //-----------------------------------
+  "canInput" : {
+    type : Boolean,
+    default : true
+  },
+  "cancelTagBubble" : {
+    type : Boolean,
+    default : false
+  },
+  //-----------------------------------
+  // Aspect
+  //-----------------------------------
+  "tagItemIconBy" : {
+    type : [String, Function],
+    default : undefined
+  },
+  "tagItemDefaultIcon" : {
+    type : String,
+    default : undefined
+  },
+  "tagOptionDefaultIcon" : {
+    type : String,
+    default : undefined
+  }
+}
