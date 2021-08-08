25618e512255f1bb5cb2666e8d9dcd17a52da451
Peter Zhang <zozohtnt@gmail.com>
2021-08-08 21:47:59 +0800

    ...

--- src/dist/es6/ti-core.js
+++ src/dist/es6/ti-core.js
@@ -1,4 +1,4 @@
-// Pack At: 2021-08-08 21:36:26
+// Pack At: 2021-08-08 21:47:41
 //##################################################
 // # import {Alert}   from "./ti-alert.mjs"
 const {Alert} = (function(){
@@ -16487,7 +16487,7 @@ function MatchCache(url) {
 }
 //---------------------------------------
 const ENV = {
-  "version" : "1.6-20210808.213626",
+  "version" : "1.6-20210808.214741",
   "dev" : false,
   "appName" : null,
   "session" : {},
--- src/dist/es6/ti-walnut.js
+++ src/dist/es6/ti-walnut.js
@@ -1,4 +1,4 @@
-// Pack At: 2021-08-08 21:36:26
+// Pack At: 2021-08-08 21:47:41
 //##################################################
 // # import Io      from "./wn-io.mjs"
 const Io = (function(){
@@ -4071,7 +4071,7 @@ const FbAlbum = (function(){
 })();
 
 //---------------------------------------
-const WALNUT_VERSION = "1.2-20210808.213627"
+const WALNUT_VERSION = "1.2-20210808.214741"
 //---------------------------------------
 // For Wn.Sys.exec command result callback
 const HOOKs = {
