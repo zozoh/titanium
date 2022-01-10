e5d9530bb5a19e7054fa1b8203917bc9293234bd
Peter Zhang <zozohtnt@gmail.com>
2022-01-11 01:05:48 +0800

    design fot TiSheetTable

--- src/core/num.mjs
+++ src/core/num.mjs
@@ -1,4 +1,6 @@
 //-----------------------------------
+const BASE26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//-----------------------------------
 const TiNum = {
   /***
    * Fill array from given number. 
@@ -11,11 +13,11 @@ const TiNum = {
    * 
    * @return the source array passed in
    */
-  fillSteps(startValue=0, len=1, {
-    ary=[], step=1
-  }={}){
-    for(let i=0; i<len; i++) {
-      ary[i] = startValue + i*step
+  fillSteps(startValue = 0, len = 1, {
+    ary = [], step = 1
+  } = {}) {
+    for (let i = 0; i < len; i++) {
+      ary[i] = startValue + i * step
     }
     return ary
   },
@@ -34,8 +36,8 @@ const TiNum = {
    * scrollIndex(-5, 5) => 0
    * ```
    */
-  scrollIndex(index, len=0) {
-    if(len > 0) {
+  scrollIndex(index, len = 0) {
+    if (len > 0) {
       let md = index % len;
       return md >= 0
         ? md
@@ -49,10 +51,10 @@ const TiNum = {
    * 
    * @return The number after tidy
    */
-  precise(n, p=2) {
+  precise(n, p = 2) {
     if (p >= 0) {
-        var y = Math.pow(10, p);
-        return Math.round(n * y) / y;
+      var y = Math.pow(10, p);
+      return Math.round(n * y) / y;
     }
     return n;
   },
@@ -62,8 +64,8 @@ const TiNum = {
    * 
    * @return new ceil value for unit
    */
-  ceilUnit(v, unit=0) {
-    if(_.isNumber(v) && unit > 0) {
+  ceilUnit(v, unit = 0) {
+    if (_.isNumber(v) && unit > 0) {
       let n = Math.ceil(v / unit)
       return n * unit
     }
@@ -75,13 +77,91 @@ const TiNum = {
    * 
    * @return new floor value for unit
    */
-  floorUnit(v, unit=0) {
-    if(_.isNumber(v) && unit > 0) {
+  floorUnit(v, unit = 0) {
+    if (_.isNumber(v) && unit > 0) {
       let n = Math.floor(v / unit)
       return n * unit
     }
     return v
-  }
+  },
+  /***
+   * Translate decimal (0-9) to 27 base system (A-Z)
+   * 
+   * ```bash
+   * #---------------------------------------
+   * # 26 base system (A-Z)
+   * 0  1  2  3  4  5  6  7  8  9
+   * A  B  C  D  E  F  G  H  I  J
+   *
+   * 10 11 12 13 14 15 16 17 18 19
+   * K  L  M  N  O  P  Q  R  S  T
+   *
+   * 20 21 22 23 24 25 26 27 28 29
+   * U  V  W  X  Y  Z  AA AB AC AD
+   * 
+   * 30 31 33 33 34 35 36 37 38 39
+   * AE AF AG AH AI AJ AK AL AM AN
+   * 
+   * 40 41 44 44 44 45 46 47 48 49
+   * AO AP AQ AR AS AT AU AV AW AX
+   * 
+   * 50 51 55 55 55 55 56 57 58 59
+   * AY AZ BA BB BC BD BE BF BG BH
+   * #---------------------------------------
+   * {high} --> "AB" <-- {low}
+   * ```
+   *
+   */
+  toBase26(n) {
+    n = Math.abs(Math.round(n))
+    let re = []
+    while (n >= 26) {
+      let high = parseInt(n / 26)
+      let low = parseInt(n - (high * 26))
+      re.push(BASE26[low])
+      n = high - 1
+    }
+    re.push(BASE26[n])
+    return re.reverse().join("")
+  },
+  /***
+   * Translate 27 base system (A-Z) to decimal (0-9)
+   */
+  fromBase26(base26) {
+    // Reverse the code from low to high
+    //  "ADC" => "C","D","A"
+    //console.log("fromBase26:", base26)
+    let cs = _.trim(base26).toUpperCase().split("").reverse().join("")
+    let n = 0;
+    let len = cs.length
+    let r = 1
+    for (let i = 0; i < len; i++) {
+      let cc = cs.charCodeAt(i)
+      // Char code 'A' == 65, 'Z' == 90
+      if (cc < 65 || cc > 90) {
+        throw `Invalid base26 number : ${base26}`
+      }
+      let bn = (cc - 65)
+      if (i > 0) {
+        bn += 1
+      }
+      n += bn * r
+      // Move higher 
+      r *= 26
+    }
+    return n
+  },
+  // _test_base_26() {
+  //   for(let i=0; i< 10000; i++) {
+  //     let bs = TiNum.toBase26(i)
+  //     let bn = TiNum.fromBase26(bs)
+  //     if(i != bn) {
+  //       console.error(`${i}. ${bs} => ${bn}`)
+  //     } else {
+  //       console.log(`${i}. ${bs} => ${bn}`)
+  //     }
+  //   }
+  // }
 }
 //---------------------------------------
 export const Num = TiNum
