f975a8af933c9b854f6ae06a6d8a8035936df4bb
Peter Zhang <zozohtnt@gmail.com>
2023-12-24 23:48:10 +0800

    ...

--- src/dist/es6/ti-walnut.js
+++ src/dist/es6/ti-walnut.js
@@ -1,4 +1,4 @@
-// Pack At: 2023-12-17 23:06:53
+// Pack At: 2023-12-24 23:44:08
 //##################################################
 // # import Io from "./wn-io.mjs"
 const Io = (function(){
@@ -4621,7 +4621,7 @@ const FbAlbum = (function(){
 })();
 
 //---------------------------------------
-const WALNUT_VERSION = "1.2-20231217.230655"
+const WALNUT_VERSION = "1.2-20231224.234408"
 //---------------------------------------
 // For Wn.Sys.exec command result callback
 const HOOKs = {
